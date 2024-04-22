f76183038909de3404de96547e476b2aea4f97bc
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-04-22 12:21:32 -0400

    feat: add hello world

---
+++ index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Hello = () => {
+    return (
+        <React.Fragment>
+            <div>
+                Hello World
+            </div>
+        </React.Fragment>
+    )
+}

28b88d13976f01ca593cb5da7d4d14f5814cf014
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-04-22 13:27:49 -0400

    test: hooks

--- index.jsx
+++ index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const Hello = () => {
-	return <React.Fragment>Hello World</React.Fragment>;
+	return <React.Fragment>
+		Hello World</React.Fragment>;
 };
 
 export { Hello };

b5936d15bacdf6e516586fe2dfba5a9fd307c4c2
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-04-22 22:45:42 -0400

    fix: errors upload

---
+++ example/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { WhatsAppButton } from '../../build';
+
+function App() {
+	return (
+		<React.Fragment>
+			<WhatsAppButton />
+		</React.Fragment>
+	);
+}
+
+export default App;
--- index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { WhatsAppButton } from './src';
-
-const Hello = () => {
-	return <React.Fragment>Hello World</React.Fragment>;
-};
-
-export { Hello, WhatsAppButton };
